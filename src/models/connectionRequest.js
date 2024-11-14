357778f64d729558b8751c102e4e5f0ddc217041
v1v4k <viivekreddi@outlook.com>
2024-11-13 21:15:12 -0600

    SendConnectionRequest API and it's validations

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -14,13 +14,22 @@ const connectionReqSchema = new mongoose.Schema(
         type : String,
         enum : {
             values:["ignored", "interested", "accepted", "rejected"],
-            message : `${VALUE} is incorrect status type`
+            message : `{VALUE} is incorrect status type`
         }
     }
 },
 {timestamps: true}
 )
 
-const ConnectionReqModel = new mongoose.Model("ConnectionReqModel", connectionReqSchema);
+connectionReqSchema.pre("save", function (next) {
+    const connectionRequest = this;
+    
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error(`Invalid Connection request`)
+    }
+    next();
+})
+
+const ConnectionReqModel = new mongoose.model("ConnectionReqModel", connectionReqSchema);
 
 module.exports = ConnectionReqModel;
--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const requestRouter = express.Router();
 const { userAuth } = require('../middlewares/auth');
+const ConnectionReqModel = require('../models/connectionRequest');
+const UserModel = require('../models/user');
 
 // sendConnectionRequest API
-requestRouter.post("/sendConnectionRequest",userAuth, async(req, res)=>{
+requestRouter.post("/sendConnectionRequest/:status/:toUserId",userAuth, async(req, res)=>{
     try{
-        const user = req.user;
-        res.send(`${user.firstName} sent the connection request`);
 
+        const user = req.user;
+        const fromUserId = user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const statusAllowed = ["interested","ignored"];
+
+        const isStatusAllowed = statusAllowed.includes(status);
+
+        if(!isStatusAllowed){
+            throw new Error(`Invalid connection status type : ${status}`);
+        }
+
+        const toUser = await UserModel.findById(toUserId);
+
+        if(!toUser){
+            throw new Error(` Sending request to the user that doesn't exist`);
+        }
+
+
+        const connectionExists = await ConnectionReqModel.findOne({
+            $or: [
+                {fromUserId, toUserId},
+                {fromUserId : toUserId, toUserId : fromUserId}
+            ]
+        })
+
+        if(connectionExists){
+            throw new Error(`Connection already exists`);
+        }
+
+
+        const ConnectionRequest = new ConnectionReqModel({fromUserId, toUserId, status});
+
+        const data = await ConnectionRequest.save();
+        
+
+        res.json({
+            message : `${req.user.firstName} to ${toUser.firstName} : ${status}`
+        })
     }
     catch(error){
         res.status(400).send(`Error : ${error.message}`)
