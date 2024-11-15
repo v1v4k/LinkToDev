ddeda33da0944647140a2dabefbe7724f45b842c
v1v4k <viivekreddi@outlook.com>
2024-11-15 00:03:50 -0600

    implemented pagination

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -64,6 +64,11 @@ userRouter.get("/user/feed", userAuth, async (req,res)=>{
     try{
         const {_id : loggedInUserId} = req.user;
 
+        const page = parseInt(req.query.limit) || 1 ;
+        let limit = parseInt(req.query.limit) || 10 ;
+        limit = limit > 50 ? 50 : limit ;
+        const skip = (page-1) * limit ;
+
         const allConnections = await ConnectionReqModel.find({
             $or : [
                 { fromUserId : loggedInUserId}, { toUserId : loggedInUserId}
@@ -84,6 +89,8 @@ userRouter.get("/user/feed", userAuth, async (req,res)=>{
         const userFeed =await UserModel.find({
             _id : {$nin : Array.from(hideUsersFromFeed) }
         }).select(SHOW_USER_DATA)
+        .skip(skip)
+        .limit(limit);
 
         res.json({
             message : `Successfully fetched the feed `,
