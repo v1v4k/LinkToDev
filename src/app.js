ea37b1aa8617ad412a2c4df51c9c08051f803807
v1v4k <viivekreddi@outlook.com>
2024-11-19 16:33:26 -0600

    added configuration in cors middleware for whitelisting

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ const authRouter  = require('./routes/authRouter');
 const  profileRouter  = require('./routes/profileRouter');
 const  requestRouter  = require('./routes/requestRouter');
 const { userRouter } = require('./routes/userRouter');
+const cors = require('cors')
 
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
