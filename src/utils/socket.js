521e7a0b3809badb8474a5c4922355652dcd457c
v1v4k <viivekreddi@outlook.com>
2025-02-04 18:46:50 -0600

    emit "sendMessage" event to room

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -7,9 +7,24 @@ const intializeSocket = (server) => {
     },
   });
 
+  // Listen for incoming socket connections
   io.on("connection", (socket) => {
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    // Listen for a user joining a private chat room
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      const roomId = [userId, targetUserId].sort().join("$"); // Generate a unique room ID for the two users
+      console.log(`${firstName} joined room : ${roomId}`);
+      socket.join(roomId); // Join the specified chat room
+    });
+
+    // Listen for a message sent by a user
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = [userId, targetUserId].sort().join("$");
+      console.log(firstName + text);
+
+      //emit an event to users in the room
+      io.to(roomId).emit("messageReceived", { firstName, text });
+    });
+
     socket.on("disconnect", () => {});
   });
 };
